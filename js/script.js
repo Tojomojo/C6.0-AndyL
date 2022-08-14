d4fde325a2882dfa060c23755735808e9242f180
Ancandy <liuya@uci.edu>
2022-08-13 20:09:26 -0700

    first draft

--- js/script.js
+++ js/script.js
@@ -1,6 +1,50 @@
 var weather = {
-    apiKey: "API Key goes here",
-    fetch(
-        
-    )
-}
+    apiKey: "edf7fec7ac2e5e83eded6c4841ba66a0",
+    fetchWeather: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + "edf7fec7ac2e5e83eded6c4841ba66a0"
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    // throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.main;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        const { UV } = data.UV;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + ".°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.querySelector(".UV").innerText = "UV Index: " + UV;
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search-bar").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+
+weather.fetchWeather("Denver");
